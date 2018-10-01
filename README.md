7d68a0345adb664bc9c800579f4f7b85db429cd5
Brad Penney <penney.brad@gmail.com>
2018-10-01 10:19:15 -0300

    updated README, added comments to scripts

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # BASHScripts
+
+This is a simple repository of some BASH scripts I've found through some online courses or written myself.  Credit is given whenever I can determine who wrote the script originally.
---
+++ myBashScript.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Author: Brad Penney
+# Purpose: Show some simple BASH functionality
+# Arguments: Takes age of user
+# Date: October 1, 2018
+
+# show the user that is executing the script
+echo -n "I am executing this script as: "
+whoami
+
+# show the current directory
+echo -n "My current directory is: "
+pwd
+
+
+# simple if statement that determines if user is old enough to watch show
+AGE=$1
+if [ ${AGE} -lt 18 ]; 
+then
+	echo "You must be 18 or older to see this movie!"
+else
+	echo "You may see the movie."
+	exit 1
+fi
+
+
